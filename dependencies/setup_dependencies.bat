530db6604b17d1ed8bcc6c58330b4ccddfe9b592
daveb-uncruise <daveb@uncruise.com>
2024-09-10 07:17:40 -0700

    more changes

--- dependencies/setup_dependencies.bat
+++ dependencies/setup_dependencies.bat
@@ -13,11 +13,11 @@ set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
 echo exporter: setting PYTHON_HOME to %PYTHON_HOME%
 
 IF "%SF_DATALOADER%" == "" (
-    set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v53.0.2
+    set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v62.0.1
     echo exporter: setting SF_DATALOADER to %SF_DATALOADER%
 ) ELSE (
     IF NOT EXIST "%SF_DATALOADER%" (
-        set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v53.0.2
+        set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v62.0.1
         echo exporter: setting SF_DATALOADER to %SF_DATALOADER%
     )
 )
