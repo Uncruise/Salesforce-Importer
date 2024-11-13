3e016750ed994428742b73fc9f3d4e7a46450157
daveb-uncruise <daveb@uncruise.com>
2024-11-13 07:34:58 -0800

    email update

--- importer.py
+++ importer.py
@@ -1022,7 +1022,7 @@ def send_email(client_emaillist, subject, file_path, emailattachments, log_path)
     # Send To Admin Only unless there is a csv file which means there was at least a load attempt and not a system failure
     sendTo_AdminOnly = True
 
-    sendTo_AdminAddress = "sfconsulting@501commons.org"
+    sendTo_AdminAddress = "daveb@adventurereadyconsulting.com"
     for sendToEmail in send_to:
         if re.search("501commons", sendToEmail, re.IGNORECASE):
             sendTo_AdminAddress = sendToEmail
