ebb55721d4eeb9000e14e7e3c6a2e48b97189415
daveb-uncruise <daveb@uncruise.com>
2024-11-14 07:57:24 -0800

    updates

---
+++ Clients/SEAWARE-BOOKINGS/importer.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set IMPORT_WAITTIME=-waittime 60
+set IMPORT_NOREFRESH=
+set IMPORT_NOUPDATE=
+set IMPORT_NOEXPORTPOSTGRES=-noexportpostgres
+set IMPORT_NOEXPORTODBC=-noexportodbc
+set IMPORT_NOEXPORTSF=-noexportsf
+set IMPORT_INSERTATTEMPTS=
+set IMPORT_EMAILATTACHMENTS=-emailattachments
+set IMPORT_INTERACTIVEMODE=-interactivemode
+set IMPORT_DISPLAYALERTS=-displayalerts
+
+call "%IMPORTER_DIRECTORY%\..\importer.bat" 
